fab698ae7fb28f3ceedaab90fa262698db0e1c84
jonas-martinez <cbobbxta.fr@gmail.com>
2021-09-28 09:55:18 +0200

    feat: StyledContainer implementation

---
+++ lib/components/container/lenra_styled_container.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:lenra_components/layout/lenra_styled_container.dart';
+import '../lenra_component.dart';
+import '../../lenra_component_builder.dart';
+
+// TODO : generate this from annotation on LenraApplicationStyledContainer
+class LenraStyledContainerBuilder extends LenraComponentBuilder<LenraApplicationStyledContainer> {
+  @override
+  LenraApplicationStyledContainer map({child, backgroundColor}) {
+    return LenraApplicationStyledContainer(child: child, backgroundColor: backgroundColor);
+  }
+
+  Map<String, String> get propsTypes {
+    return {
+      "child": "Widget",
+      "backgroundColor": "Color",
+      "borderWidth": "double",
+      "borderColor": "Color",
+      "borderRadius": "double",
+    };
+  }
+}
+
+class LenraApplicationStyledContainer extends StatelessLenraComponent {
+  final Widget child;
+  final Color? backgroundColor;
+  final double? borderWidth;
+  final Color? borderColor;
+  final double? borderRadius;
+
+  LenraApplicationStyledContainer({
+    required this.child,
+    required this.backgroundColor,
+    this.borderWidth,
+    this.borderColor,
+    this.borderRadius,
+  }) : super();
+
+  @override
+  Widget build(BuildContext context) {
+    Border? border = null;
+
+    if (borderWidth != null || borderColor != null) {
+      border = Border.all(width: borderWidth ?? 1.0, color: borderColor ?? const Color(4278190080));
+    }
+
+    return LenraStyledContainer(
+      border: border,
+      borderRadius: BorderRadius.circular(borderRadius ?? 0),
+      color: this.backgroundColor,
+      child: child,
+    );
+  }
+}
