db15337d82ac143a274e32865f62bebc17b7e3f2
jonas-martinez <cbobbxta.fr@gmail.com>
2021-09-30 12:12:47 +0200

    WIP: Needs to handle null value on icon props

--- lib/components/actionable/lenra_button.dart
+++ lib/components/actionable/lenra_button.dart
@@ -11,13 +11,15 @@ import 'package:lenra_components/component/lenra_button.dart';
 // TODO : generate this from annotation on LenraButton
 class LenraButtonBuilder extends LenraComponentBuilder<LenraApplicationButton> {
   @override
-  LenraApplicationButton map({text, disabled, size, mainStyle, onPressed}) {
+  LenraApplicationButton map({text, disabled, size, mainStyle, onPressed, leftIcon, rightIcon}) {
     return LenraApplicationButton(
       text: text,
       disabled: disabled,
       size: size,
       mainStyle: mainStyle,
       onPressed: onPressed,
+      leftIcon: leftIcon,
+      rightIcon: rightIcon,
     );
   }
 
@@ -29,17 +31,20 @@ class LenraButtonBuilder extends LenraComponentBuilder<LenraApplicationButton> {
       "size": "String",
       "mainStyle": "String",
       "onPressed": "Map<String, dynamic>",
+      "leftIcon": "Icon",
+      "rightIcon": "Icon",
     };
   }
 }
 
-class LenraApplicationButton extends StatelessLenraComponent
-    implements LenraActionable {
+class LenraApplicationButton extends StatelessLenraComponent implements LenraActionable {
   final String text;
   final bool? disabled;
   final String? size;
   final String? mainStyle;
   final Map<String, dynamic>? onPressed;
+  final Icon leftIcon;
+  final Icon rightIcon;
 
   LenraApplicationButton({
     required this.text,
@@ -47,6 +52,8 @@ class LenraApplicationButton extends StatelessLenraComponent
     required this.size,
     required this.mainStyle,
     required this.onPressed,
+    required this.leftIcon,
+    required this.rightIcon,
   }) : super();
 
   void onButtonPressed(BuildContext context) {
@@ -63,6 +70,8 @@ class LenraApplicationButton extends StatelessLenraComponent
       size: SizeHelper.fromString(size),
       type: TypeHelper.fromString(mainStyle),
       onPressed: () => onButtonPressed(context),
+      leftIcon: leftIcon,
+      rightIcon: rightIcon,
     );
   }
 }
