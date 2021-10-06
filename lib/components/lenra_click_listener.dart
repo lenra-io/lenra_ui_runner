495ecd4e0378e9fb179bbefd569aa66ffc1beacd
jonas-martinez <cbobbxta.fr@gmail.com>
2021-10-06 09:37:00 +0200

    feat: Added click listener

---
+++ lib/components/lenra_click_listener.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:lenra_ui_runner/components/actionable/events/lenra_on_press_event.dart';
+import 'package:lenra_ui_runner/lenra_component_builder.dart';
+import 'lenra_component.dart';
+
+// TODO generate this from annotation on LenraMenu
+class LenraClickListenerBuilder extends LenraComponentBuilder<LenraApplicationClickListener> {
+  @override
+  LenraApplicationClickListener map({child, onPressed}) {
+    return LenraApplicationClickListener(child: child, onPressed: onPressed);
+  }
+
+  @override
+  Map<String, String> get propsTypes {
+    return {
+      "onPressed": "Map<String, dynamic>",
+    };
+  }
+
+  @override
+  List<String> get childKeys {
+    return ["child"];
+  }
+}
+
+class LenraApplicationClickListener extends StatelessLenraComponent {
+  final Widget child;
+  final Map<String, dynamic>? onPressed;
+
+  LenraApplicationClickListener({
+    required this.child,
+    required this.onPressed,
+  }) : super();
+
+  void onWidgetPressed(BuildContext context) {
+    if (onPressed != null && onPressed!.containsKey("code")) {
+      LenraOnPressEvent(code: onPressed!['code']!, event: {}).dispatch(context);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: InkWell(
+        child: child,
+        onTap: () => onWidgetPressed(context),
+      ),
+    );
+  }
+}
