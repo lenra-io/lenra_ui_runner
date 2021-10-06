2ac02b14dada5088be0cda6e8f4b8ee259ba122b
jonas-martinez <cbobbxta.fr@gmail.com>
2021-10-06 10:19:08 +0200

    fix comment

--- lib/components/lenra_click_listener.dart
+++ lib/components/lenra_click_listener.dart
@@ -3,7 +3,7 @@ import 'package:lenra_ui_runner/components/actionable/events/lenra_on_press_even
 import 'package:lenra_ui_runner/lenra_component_builder.dart';
 import 'lenra_component.dart';
 
-// TODO generate this from annotation on LenraMenu
+// TODO generate this from annotation on LenraClickListener
 class LenraClickListenerBuilder extends LenraComponentBuilder<LenraApplicationClickListener> {
   @override
   LenraApplicationClickListener map({child, onPressed}) {
