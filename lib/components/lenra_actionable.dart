05f01d028aa6dd83eade93ff742221cc87b82b51
jonas-martinez <cbobbxta.fr@gmail.com>
2021-10-06 14:45:16 +0200

    fix

--- lib/components/lenra_actionable.dart
+++ lib/components/lenra_actionable.dart
@@ -11,22 +11,16 @@ class LenraActionableBuilder extends LenraComponentBuilder<LenraApplicationActio
     onPressed,
     onDoublePressed,
     onLongPressed,
-    onPressedDown,
     onPressedCancel,
     onHovered,
-    onFocusChange,
-    onHighlightChanged,
   }) {
     return LenraApplicationActionable(
       child: child,
       onPressed: onPressed,
       onDoublePressed: onDoublePressed,
       onLongPressed: onLongPressed,
-      onPressedDown: onPressedDown,
       onPressedCancel: onPressedCancel,
       onHovered: onHovered,
-      onFocusChange: onFocusChange,
-      onHighlightChanged: onHighlightChanged,
     );
   }
 
@@ -36,11 +30,8 @@ class LenraActionableBuilder extends LenraComponentBuilder<LenraApplicationActio
       "onPressed": "Map<String, dynamic>",
       "onDoublePressed": "Map<String, dynamic>",
       "onLongPressed": "Map<String, dynamic>",
-      "onPressedDown": "Map<String, dynamic>",
       "onPressedCancel": "Map<String, dynamic>",
       "onHovered": "Map<String, dynamic>",
-      "onFocusChange": "Map<String, dynamic>",
-      "onHighlightChange": "Map<String, dynamic>",
     };
   }
 
@@ -55,22 +46,16 @@ class LenraApplicationActionable extends StatelessLenraComponent {
   final Map<String, dynamic>? onPressed;
   final Map<String, dynamic>? onDoublePressed;
   final Map<String, dynamic>? onLongPressed;
-  final Map<String, dynamic>? onPressedDown;
   final Map<String, dynamic>? onPressedCancel;
   final Map<String, dynamic>? onHovered;
-  final Map<String, dynamic>? onFocusChange;
-  final Map<String, dynamic>? onHighlightChanged;
 
   LenraApplicationActionable({
     required this.child,
     required this.onPressed,
     required this.onDoublePressed,
     required this.onLongPressed,
-    required this.onPressedDown,
     required this.onPressedCancel,
     required this.onHovered,
-    required this.onFocusChange,
-    required this.onHighlightChanged,
   }) : super();
 
   void onAction(Map<String, dynamic>? action, BuildContext context) {
@@ -87,11 +72,8 @@ class LenraApplicationActionable extends StatelessLenraComponent {
         onTap: () => onAction(onPressed, context),
         onDoubleTap: () => onAction(onDoublePressed, context),
         onLongPress: () => onAction(onLongPressed, context),
-        onTapDown: (e) => onAction(onPressedDown, context),
         onTapCancel: () => onAction(onPressedCancel, context),
         onHover: (e) => onAction(onHovered, context),
-        onFocusChange: (e) => onAction(onFocusChange, context),
-        onHighlightChanged: (e) => onAction(onHighlightChanged, context),
       ),
     );
   }
