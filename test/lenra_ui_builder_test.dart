8dfb6f8da0a9170ebc50005e2416e2ac9ff96bf1
jonas-martinez <cbobbxta.fr@gmail.com>
2021-10-12 09:38:15 +0200

    fix: Fixed UI Patch for addChild operation

--- test/lenra_ui_builder_test.dart
+++ test/lenra_ui_builder_test.dart
@@ -140,4 +140,51 @@ void main() {
     expect(find.text("foo"), findsNothing);
     expect(find.text("bar"), findsOneWidget);
   });
+
+  testWidgets('Change child of styledContainer', (WidgetTester tester) async {
+    StreamController<Map<String, dynamic>> uiStream = StreamController();
+    StreamController<List<Map<String, dynamic>>> patchUiStream = StreamController();
+
+    await tester.pumpWidget(
+      createBaseTestWidgets(
+        child: LenraUiBuilder(uiStream: uiStream, patchUiStream: patchUiStream),
+      ),
+    );
+
+    Map<String, dynamic> ui = {
+      "root": {
+        "type": "button",
+        "text": "foo",
+        "onPressed": {"code": "john"}
+      }
+    };
+
+    List<Map<String, dynamic>> patches = [
+      {"op": "remove", "path": "/root/onPressed"},
+      {"op": "remove", "path": "/root/text"},
+      {"op": "replace", "path": "/root/type", "value": "styledContainer"},
+      {
+        "op": "add",
+        "path": "/root/child",
+        "value": {
+          "type": "button",
+          "text": "bar",
+          "onPressed": {"code": "doe"}
+        }
+      }
+    ];
+
+    uiStream.add(ui);
+    await tester.pump();
+
+    expect(find.text("foo"), findsOneWidget);
+    expect(find.text("bar"), findsNothing);
+
+    patchUiStream.add(patches);
+    await tester.pump();
+    await tester.pump();
+
+    expect(find.text("bar"), findsOneWidget);
+    expect(find.text("foo"), findsNothing);
+  });
 }
