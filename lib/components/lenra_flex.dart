3b97015e91f2033e16cce5773ad726256a846852
jonas-martinez <cbobbxta.fr@gmail.com>
2021-10-25 11:50:01 +0200

    Switched material to widgets

--- lib/components/lenra_flex.dart
+++ lib/components/lenra_flex.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/material.dart';
-
+import 'package:flutter/widgets.dart';
 import 'package:lenra_components/layout/lenra_flex.dart';
 import 'package:lenra_ui_runner/lenra_component_builder.dart';
 
