e4f979f88a85f0ff9a50f67e1749e5e28b4dbb1c
jonas-martinez <cbobbxta.fr@gmail.com>
2021-11-08 15:16:30 +0100

    Reworked image component

--- lib/components/lenra_image.dart
+++ lib/components/lenra_image.dart
@@ -6,8 +6,42 @@ import 'package:provider/provider.dart';
 // TODO : generate this from annotation on LenraImage
 class LenraImageBuilder extends LenraComponentBuilder<LenraApplicationImage> {
   @override
-  LenraApplicationImage map({path, backgroundColor, height, width}) {
-    return LenraApplicationImage(path: path, width: width, height: height);
+  LenraApplicationImage map({
+    path,
+    height,
+    width,
+    alignment,
+    centerSlice,
+    errorBuilder,
+    excludeFromSemantics,
+    filterQuality,
+    fit,
+    frameBuilder,
+    gaplessPlayback,
+    isAntiAlias,
+    loadingBuilder,
+    opacity,
+    repeat,
+    semanticLabel,
+  }) {
+    return LenraApplicationImage(
+      path: path,
+      width: width,
+      height: height,
+      alignment: alignment,
+      centerSlice: centerSlice,
+      errorBuilder: errorBuilder,
+      excludeFromSemantics: excludeFromSemantics,
+      filterQuality: filterQuality,
+      fit: fit,
+      frameBuilder: frameBuilder,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      loadingBuilder: loadingBuilder,
+      opacity: opacity,
+      repeat: repeat,
+      semanticLabel: semanticLabel,
+    );
   }
 
   @override
@@ -22,14 +56,39 @@ class LenraImageBuilder extends LenraComponentBuilder<LenraApplicationImage> {
 
 class LenraApplicationImage extends StatelessWidget {
   final String path;
-  // TODO: For future features add handling for image resizing
   final double? height;
   final double? width;
+  final AlignmentGeometry alignment;
+  final Rect? centerSlice;
+  final ImageErrorWidgetBuilder? errorBuilder;
+  final bool excludeFromSemantics;
+  final FilterQuality filterQuality;
+  final BoxFit? fit;
+  final ImageFrameBuilder? frameBuilder;
+  final bool gaplessPlayback;
+  final bool isAntiAlias;
+  final ImageLoadingBuilder? loadingBuilder;
+  final Animation<double>? opacity;
+  final ImageRepeat repeat;
+  final String? semanticLabel;
 
   LenraApplicationImage({
     required this.path,
-    this.height,
-    this.width,
+    required this.height,
+    required this.width,
+    required this.alignment,
+    required this.centerSlice,
+    required this.errorBuilder,
+    required this.excludeFromSemantics,
+    required this.filterQuality,
+    required this.fit,
+    required this.frameBuilder,
+    required this.gaplessPlayback,
+    required this.isAntiAlias,
+    required this.loadingBuilder,
+    required this.opacity,
+    required this.repeat,
+    required this.semanticLabel,
   }) : super();
 
   @override
@@ -38,6 +97,21 @@ class LenraApplicationImage extends StatelessWidget {
 
     return Image.network(
       "${model.httpEndpoint}/api/apps/${model.applicationName}/resources/$path?token=${model.accessToken}",
+      height: height,
+      width: width,
+      alignment: alignment,
+      centerSlice: centerSlice,
+      errorBuilder: errorBuilder,
+      excludeFromSemantics: excludeFromSemantics,
+      filterQuality: filterQuality,
+      fit: fit,
+      frameBuilder: frameBuilder,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      loadingBuilder: loadingBuilder,
+      opacity: opacity,
+      repeat: repeat,
+      semanticLabel: semanticLabel,
     );
   }
 }
