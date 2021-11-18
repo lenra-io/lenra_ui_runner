d472d5625a480ac7737ace90bfd143b167e5bb11
jonas-martinez <cbobbxta.fr@gmail.com>
2021-11-18 14:27:41 +0100

    feat: Checkbox rework

--- lib/components/lenra_checkbox.dart
+++ lib/components/lenra_checkbox.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:lenra_ui_runner/components/events/on_pressed_event.dart';
+import 'package:lenra_components/theme/lenra_checkbox_style.dart';
 import 'package:lenra_ui_runner/components/listeners/listener.dart' as lenra;
 import 'package:lenra_ui_runner/lenra_component_builder.dart';
 import 'package:lenra_components/component/lenra_checkbox.dart';
@@ -7,47 +7,56 @@ import 'package:lenra_components/component/lenra_checkbox.dart';
 // TODO : generate this from annotation on LenraCheckbox
 class LenraCheckboxBuilder extends LenraComponentBuilder<LenraApplicationCheckbox> {
   @override
-  LenraApplicationCheckbox map({value, label, disabled, onPressed}) {
-    return LenraApplicationCheckbox(value: value, label: label, disabled: disabled, onPressed: onPressed);
+  LenraApplicationCheckbox map({value, tristate, onPressed, style, materialTapTargetSize, autofocus}) {
+    return LenraApplicationCheckbox(
+      value: value,
+      tristate: tristate,
+      onPressed: onPressed,
+      style: style,
+      materialTapTargetSize: materialTapTargetSize,
+      autofocus: autofocus,
+    );
   }
 
   @override
   Map<String, Type> get propsTypes {
     return {
       "value": bool,
-      "label": String,
-      "disabled": bool,
+      "tristate": bool,
       "onPressed": Listener,
+      "style": LenraCheckboxStyle,
+      "materialTapTargetSize": MaterialTapTargetSize,
+      "autofocus": bool,
     };
   }
 }
 
 class LenraApplicationCheckbox extends StatelessWidget {
   final bool value;
-  final String? label;
-  final bool? disabled;
+  final bool? tristate;
   final lenra.Listener? onPressed;
+  final LenraCheckboxStyle style;
+  final MaterialTapTargetSize materialTapTargetSize;
+  final bool? autofocus;
 
   LenraApplicationCheckbox({
     required this.value,
-    required this.label,
-    required this.disabled,
+    required this.tristate,
     required this.onPressed,
+    required this.style,
+    required this.materialTapTargetSize,
+    required this.autofocus,
   }) : super();
-
-  void onCheckboxPressed(BuildContext context) {
-    if (onPressed != null) {
-      OnPressedEvent(code: onPressed!.code).dispatch(context);
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
     return LenraCheckbox(
       value: value,
-      label: label,
-      disabled: disabled ?? false,
-      onPressed: () => onCheckboxPressed(context),
+      tristate: tristate ?? false,
+      onPressed: onPressed,
+      style: style,
+      materialTapTargetSize: materialTapTargetSize,
+      autofocus: autofocus ?? false,
     );
   }
 }
