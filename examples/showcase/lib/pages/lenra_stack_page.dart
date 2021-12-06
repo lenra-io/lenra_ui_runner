235e2958a18ad3ade8599b409b0d1cc1e778b427
jonas-martinez <cbobbxta.fr@gmail.com>
2021-12-06 15:40:43 +0100

    Added example

---
+++ examples/showcase/lib/pages/lenra_stack_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:lenra_ui_runner/components/events/event.dart';
+import 'package:showcase/ui_builder.dart';
+
+class LenraStackPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _LenraStackPageState();
+  }
+}
+
+class _LenraStackPageState extends UiBuilderState<LenraStackPage, bool> {
+  @override
+  Map<String, dynamic> get ui {
+    return {
+      "root": {
+        "type": "stack",
+        "children": [
+          {
+            "type": "container",
+            "decoration": {"color": 0xFFFF0000},
+            "constraints": {
+              "minWidth": 100,
+              "maxWidth": 100,
+              "minHeight": 100,
+              "maxHeight": 100,
+            },
+            "child": {"type": "text", "value": ""}
+          },
+          {
+            "type": "container",
+            "decoration": {"color": 0xFF00FF00},
+            "constraints": {
+              "minWidth": 60,
+              "maxWidth": 60,
+              "minHeight": 60,
+              "maxHeight": 60,
+            },
+            "child": {"type": "text", "value": "This is stacked."}
+          },
+        ]
+      }
+    };
+  }
+
+  @override
+  getData(Event event) {
+    switch (event.code) {
+      case "InitData":
+        return true;
+      default:
+        return false;
+    }
+  }
+}
