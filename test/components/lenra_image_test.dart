41cd0e09347fcc1a8defc4d19d84c46a2d72a132
jonas-martinez <cbobbxta.fr@gmail.com>
2021-12-21 15:31:35 +0100

    Added mocks

--- test/components/lenra_image_test.dart
+++ test/components/lenra_image_test.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
 import 'package:flutter/material.dart';
@@ -5,78 +7,25 @@ import 'package:lenra_ui_runner/components/lenra_image.dart';
 import 'package:lenra_ui_runner/lenra_widget.dart';
 import 'package:lenra_ui_runner/widget_model.dart';
 import 'package:mockito/mockito.dart';
+import 'package:mockito/annotations.dart';
+
 import 'package:provider/provider.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'lenra_image_test.mocks.dart';
 
 import '../test_helper.dart';
 
-final Uint8List kTransparentImage = Uint8List.fromList(<int>[
-  0x89,
-  0x50,
-  0x4E,
-  0x47,
-  0x0D,
-  0x0A,
-  0x1A,
-  0x0A,
-  0x00,
-  0x00,
-  0x00,
-  0x0D,
-  0x49,
-  0x48,
-  0x44,
-  0x52,
-  0x00,
-  0x00,
-  0x00,
-  0x01,
-  0x00,
-  0x00,
-  0x00,
-  0x01,
-  0x08,
-  0x06,
-  0x00,
-  0x00,
-  0x00,
-  0x1F,
-  0x15,
-  0xC4,
-  0x89,
-  0x00,
-  0x00,
-  0x00,
-  0x0A,
-  0x49,
-  0x44,
-  0x41,
-  0x54,
-  0x78,
-  0x9C,
-  0x63,
-  0x00,
-  0x01,
-  0x00,
-  0x00,
-  0x05,
-  0x00,
-  0x01,
-  0x0D,
-  0x0A,
-  0x2D,
-  0xB4,
-  0x00,
-  0x00,
-  0x00,
-  0x00,
-  0x49,
-  0x45,
-  0x4E,
-  0x44,
-  0xAE,
-]);
+/// One pixel image
+Uint8List _opImage =
+    base64.decode('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII=');
 
+final MockHttpClient client = MockHttpClient();
+final MockHttpClientRequest request = MockHttpClientRequest();
+final MockHttpClientResponse response = MockHttpClientResponse();
+final MockHttpHeaders headers = MockHttpHeaders();
+
+@GenerateMocks([HttpClient, HttpClientRequest, HttpHeaders], customMocks: [MockSpec<HttpClientResponse>()])
 void main() {
   // testWidgets('LenraImage errorBuilder should build error widget when an error occurs.', (WidgetTester tester) async {
   //   BuildContext? _context;
@@ -109,11 +58,6 @@ void main() {
   //   await tester.pumpAndSettle();
   //   expect(find.text("Error"), findsOneWidget);
   // });
-
-  final MockHttpClient client = MockHttpClient();
-  final MockHttpClientRequest request = MockHttpClientRequest();
-  final MockHttpClientResponse response = MockHttpClientResponse();
-  final MockHttpHeaders headers = MockHttpHeaders();
 
   testWidgets('LenraImage loadingBuilder should build loader widget when the image is loading.',
       (WidgetTester tester) async {
@@ -150,34 +94,21 @@ void main() {
       expect(find.byType(LenraApplicationImage), findsOneWidget);
       expect(find.text("Loading"), findsOneWidget);
     }, createHttpClient: (_) {
-      print("CREATE HTTP CLIENT");
-
-      when(client.getUrl(Uri.parse("long-to-load-image"))).thenAnswer((_) {
-        print("THEN ANSWER GETURL");
-        print(Future<HttpClientRequest>.value(request));
-        return Future<HttpClientRequest>.value(request);
-      });
+      when(client.getUrl(any)).thenAnswer((_) => Future<HttpClientRequest>.value(request));
       when(request.headers).thenReturn(headers);
       when(request.close()).thenAnswer((_) => Future<HttpClientResponse>.value(response));
-      when(response.contentLength).thenReturn(kTransparentImage.length);
+      when(response.contentLength).thenReturn(_opImage.length);
       when(response.statusCode).thenReturn(HttpStatus.ok);
+      when(response.compressionState).thenReturn(HttpClientResponseCompressionState.notCompressed);
       when(response.listen(any)).thenAnswer((Invocation invocation) {
         final void Function(List<int>) onData = invocation.positionalArguments[0];
         final void Function() onDone = invocation.namedArguments[#onDone];
         final void Function(Object, [StackTrace]) onError = invocation.namedArguments[#onError];
         final bool cancelOnError = invocation.namedArguments[#cancelOnError];
-        return Stream<List<int>>.fromIterable(<List<int>>[kTransparentImage])
+        return Stream<List<int>>.fromIterable(<List<int>>[_opImage])
             .listen(onData, onDone: onDone, onError: onError, cancelOnError: cancelOnError);
       });
       return client;
     });
   });
 }
-
-class MockHttpClient extends Mock implements HttpClient {}
-
-class MockHttpClientRequest extends Mock implements HttpClientRequest {}
-
-class MockHttpClientResponse extends Mock implements HttpClientResponse {}
-
-class MockHttpHeaders extends Mock implements HttpHeaders {}
