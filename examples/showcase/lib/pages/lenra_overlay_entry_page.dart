60dfcec8b4d340234dbcc20bb89dccbcaa60ea07
jonas-martinez <cbobbxta.fr@gmail.com>
2022-03-22 16:22:25 +0100

    Better example

--- examples/showcase/lib/pages/lenra_overlay_entry_page.dart
+++ examples/showcase/lib/pages/lenra_overlay_entry_page.dart
@@ -55,6 +55,32 @@ class _LenraOverlayEntryPageState extends UiBuilderState<LenraOverlayEntryPage, 
               },
             }
           },
+          {
+            "type": "overlayEntry",
+            "showOverlay": data.containsKey("showOverlay2") ? data["showOverlay2"] : true,
+            "child": {
+              "type": "container",
+              "decoration": {
+                "color": 0x44000000,
+              },
+              "child": {
+                "type": "flex",
+                "mainAxisAlignment": "center",
+                "crossAxisAlignment": "center",
+                "children": [
+                  {"type": "text", "value": "This is an overlay."},
+                  {"type": "text", "value": "You can close it by clicking on the button."},
+                  {
+                    "type": "button",
+                    "text": "Close",
+                    "onPressed": {
+                      "code": "hideOverlay2",
+                    }
+                  }
+                ]
+              },
+            }
+          },
         ]
       }
     };
@@ -63,15 +89,22 @@ class _LenraOverlayEntryPageState extends UiBuilderState<LenraOverlayEntryPage, 
   @override
   Map<String, dynamic> getData(Event event) {
     if (event.code == "InitData") {
-      return {"showOverlay": false};
+      return {
+        "showOverlay": false,
+        "showOverlay2": true,
+      };
     }
 
     if (event.code == "showOverlay") {
-      return {"showOverlay": true};
+      return {"showOverlay": true, "showOverlay2": false};
     }
 
     if (event.code == "hideOverlay") {
-      return {"showOverlay": false};
+      return {"showOverlay": false, "showOverlay2": false};
+    }
+
+    if (event.code == "hideOverlay2") {
+      return {"showOverlay2": false};
     }
 
     return event.data.toMap();
