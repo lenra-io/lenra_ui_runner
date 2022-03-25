c47bbdae1c00107181c8a2b3883801fc5b552fe7
jonas-martinez <cbobbxta.fr@gmail.com>
2022-03-25 11:41:45 +0100

    Complex example but this is the only one working

--- examples/showcase/lib/pages/lenra_overlay_entry_page.dart
+++ examples/showcase/lib/pages/lenra_overlay_entry_page.dart
@@ -11,335 +11,276 @@ class LenraOverlayEntryPage extends StatefulWidget {
 }
 
 class _LenraOverlayEntryPageState extends UiBuilderState<LenraOverlayEntryPage, Map<String, dynamic>> {
+  Map<String, dynamic> oldData = {
+    "navigation": {"page": "home"},
+    "data": {}
+  };
+
   @override
   Map<String, dynamic> getUi(dynamic data) {
-    return {
-      "root": data.containsKey("NewPage")
-          ? data["NewPage"]
-          : {
-              "type": "flex",
-              "fillParent": true,
-              "mainAxisAlignment": "center",
-              "crossAxisAlignment": "center",
-              "children": [
-                {
-                  "type": "flex",
-                  "mainAxisAlignment": "center",
-                  "crossAxisAlignment": "center",
-                  "direction": "vertical",
-                  "spacing": 2,
-                  "fillParent": true,
-                  "children": [
-                    {
-                      "type": "text",
-                      "value": "You can show another overlay by clicking on the button below.",
-                    },
-                    {
-                      "type": "button",
-                      "text": "Show",
-                      "onPressed": {
-                        "code": "showOverlay",
-                      }
-                    },
-                  ]
-                },
-                {
-                  "type": "overlayEntry",
-                  "showOverlay": data.containsKey("showOverlay") ? data["showOverlay"] : false,
-                  "child": {
-                    "type": "container",
-                    "decoration": {
-                      "color": 0x44000000,
-                    },
-                    "child": {
-                      "type": "flex",
-                      "mainAxisAlignment": "center",
-                      "crossAxisAlignment": "center",
-                      "direction": "vertical",
-                      "spacing": 2,
-                      "children": [
-                        {
-                          "type": "container",
-                          "decoration": {"color": 0xFFFFFFFF},
-                          "padding": {
-                            "left": 2,
-                            "right": 2,
-                            "top": 2,
-                            "bottom": 2,
-                          },
-                          "child": {
-                            "type": "flex",
-                            "crossAxisAlignment": "center",
-                            "direction": "vertical",
-                            "spacing": 2,
-                            "children": [
-                              {"type": "text", "value": "This is an overlay entry."},
-                              {
-                                "type": "flex",
-                                "direction": "horizontal",
-                                "children": [
-                                  {
-                                    "type": "textfield",
-                                    "value": data["textfieldValue"],
-                                    "label": "Quel est votre Prénom ?",
-                                    "hintText": "Pascal",
-                                    "minLines": 3,
-                                    "maxLines": 5,
-                                    "size": "large",
-                                    "onChanged": {"code": "CodeA"}
-                                  },
-                                  {"type": "text", "value": data["textfieldValue"]}
-                                ]
-                              },
-                              {
-                                "type": "button",
-                                "text": "New page",
-                                "onPressed": {
-                                  "code": "NewPage",
-                                }
-                              },
-                              {
-                                "type": "button",
-                                "text": "Close",
-                                "onPressed": {
-                                  "code": "hideOverlay",
-                                }
-                              }
-                            ],
-                          },
-                        },
-                      ]
-                    },
-                  }
-                },
-                {
-                  "type": "overlayEntry",
-                  "showOverlay": data.containsKey("showOverlay2") ? data["showOverlay2"] : true,
-                  "child": {
-                    "type": "container",
-                    "decoration": {
-                      "color": 0x44000000,
-                    },
-                    "child": {
-                      "type": "flex",
-                      "mainAxisAlignment": "center",
-                      "crossAxisAlignment": "center",
-                      "direction": "vertical",
-                      "children": [
-                        {
-                          "type": "container",
-                          "decoration": {"color": 0xFFFFFFFF},
-                          "padding": {
-                            "left": 2,
-                            "right": 2,
-                            "top": 2,
-                            "bottom": 2,
-                          },
-                          "child": {
-                            "type": "flex",
-                            "crossAxisAlignment": "center",
-                            "direction": "vertical",
-                            "spacing": 2,
-                            "children": [
-                              {
-                                "type": "text",
-                                "value": "This is an overlay.",
-                              },
-                              {"type": "text", "value": "You can close it by clicking on the button."},
-                              {
-                                "type": "button",
-                                "text": "Close",
-                                "onPressed": {
-                                  "code": "hideOverlay2",
-                                }
-                              }
-                            ]
-                          }
-                        },
-                      ]
-                    },
-                  }
-                },
-              ]
-            }
-    };
+    return {"root": getPage(data["navigation"]["page"], data)};
   }
 
   @override
   Map<String, dynamic> getData(Event event) {
+    Map<String, dynamic> navigation = {"page": oldData["navigation"]["page"]};
+
+    Map<String, dynamic> data = {
+      "textfieldValue": oldData["data"].containsKey("textfieldValue") ? oldData["data"]["textfieldValue"] : ""
+    };
+
     if (event.code == "NewPage") {
-      return {
-        "textfieldValue": "",
+      navigation.addAll({"page": "newPage"});
+      data.addAll({
         "showOverlay": true,
-        "NewPage": {
-          "type": "flex",
-          "fillParent": true,
-          "mainAxisAlignment": "center",
-          "crossAxisAlignment": "center",
-          "children": [
-            {
-              "type": "flex",
-              "mainAxisAlignment": "center",
-              "crossAxisAlignment": "center",
-              "direction": "vertical",
-              "spacing": 2,
-              "fillParent": true,
-              "children": [
-                {
-                  "type": "text",
-                  "value": "This is another page.",
-                },
-                {
-                  "type": "button",
-                  "text": "Go back to the first page",
-                  "onPressed": {
-                    "code": "InitData",
-                  }
-                },
-              ]
-            },
-            {
-                  "type": "overlayEntry",
-                  "showOverlay": data.containsKey("showOverlay") ? data["showOverlay"] : false,
-                  "child": {
-                    "type": "container",
-                    "decoration": {
-                      "color": 0x44000000,
-                    },
-                    "child": {
-                      "type": "flex",
-                      "mainAxisAlignment": "center",
-                      "crossAxisAlignment": "center",
-                      "direction": "vertical",
-                      "spacing": 2,
-                      "children": [
-                        {
-                          "type": "container",
-                          "decoration": {"color": 0xFFFFFFFF},
-                          "padding": {
-                            "left": 2,
-                            "right": 2,
-                            "top": 2,
-                            "bottom": 2,
-                          },
-                          "child": {
-                            "type": "flex",
-                            "crossAxisAlignment": "center",
-                            "direction": "vertical",
-                            "spacing": 2,
-                            "children": [
-                              {"type": "text", "value": "This is an overlay entry."},
-                              {
-                                "type": "flex",
-                                "direction": "horizontal",
-                                "children": [
-                                  {
-                                    "type": "textfield",
-                                    "value": data["textfieldValue"],
-                                    "label": "Quel est votre Prénom ?",
-                                    "hintText": "Pascal",
-                                    "minLines": 3,
-                                    "maxLines": 5,
-                                    "size": "large",
-                                    "onChanged": {"code": "CodeA"}
-                                  },
-                                  {"type": "text", "value": data["textfieldValue"]}
-                                ]
-                              },
-                              {
-                                "type": "button",
-                                "text": "New page",
-                                "onPressed": {
-                                  "code": "NewPage",
-                                }
-                              },
-                              {
-                                "type": "button",
-                                "text": "Close",
-                                "onPressed": {
-                                  "code": "hideOverlay",
-                                }
-                              }
-                            ],
-                          },
-                        },
-                      ]
-                    },
-                  }
-                },
-          ]
-        }
-      };
+        "showOverlay2": false,
+      });
     }
 
     if (event.code == "InitData") {
-      return {
-        "textfieldValue": "",
+      navigation.addAll({"page": "home"});
+      data.addAll({
         "showOverlay": false,
         "showOverlay2": true,
-      };
+      });
     }
 
     if (event.code == "showOverlay") {
-      return {
+      data.addAll({
         "showOverlay": true,
         "showOverlay2": false,
-        "textfieldValue": "",
-      };
+      });
     }
 
     if (event.code == "hideOverlay") {
-      return {
-        "textfieldValue": "",
+      data.addAll({
         "showOverlay": false,
         "showOverlay2": false,
-        "NewPage": {
-          "type": "flex",
-          "fillParent": true,
-          "mainAxisAlignment": "center",
-          "crossAxisAlignment": "center",
-          "children": [
-            {
-              "type": "flex",
-              "mainAxisAlignment": "center",
-              "crossAxisAlignment": "center",
-              "direction": "vertical",
-              "spacing": 2,
-              "fillParent": true,
-              "children": [
-                {
-                  "type": "text",
-                  "value": "This is another page.",
-                },
-                {
-                  "type": "button",
-                  "text": "Go back to the first page",
-                  "onPressed": {
-                    "code": "InitData",
-                  }
-                },
-              ]
-            },
-          ]
-        }
-      };
+      });
     }
 
     if (event.code == "CodeA") {
-      return {
+      data.addAll({
         "textfieldValue": (event as OnChangedEvent).data.value,
         "showOverlay": true,
         "showOverlay2": false,
-      };
+      });
     }
 
     if (event.code == "hideOverlay2") {
-      return {
+      data.addAll({
         "showOverlay2": false,
-        "textfieldValue": "",
-      };
+      });
     }
 
-    return event.data.toMap();
+    Map<String, dynamic> res = {"navigation": navigation, "data": data};
+    oldData = res;
+    return res;
+  }
+
+  List getOverlays(Map<String, dynamic> data) {
+    return [
+      {
+        "type": "overlayEntry",
+        "showOverlay": data["data"].containsKey("showOverlay") ? data["data"]["showOverlay"] : false,
+        "child": {
+          "type": "container",
+          "decoration": {
+            "color": 0x44000000,
+          },
+          "child": {
+            "type": "flex",
+            "mainAxisAlignment": "center",
+            "crossAxisAlignment": "center",
+            "direction": "vertical",
+            "spacing": 2,
+            "children": [
+              {
+                "type": "container",
+                "decoration": {"color": 0xFFFFFFFF},
+                "padding": {
+                  "left": 2,
+                  "right": 2,
+                  "top": 2,
+                  "bottom": 2,
+                },
+                "child": {
+                  "type": "flex",
+                  "crossAxisAlignment": "center",
+                  "direction": "vertical",
+                  "spacing": 2,
+                  "children": [
+                    {"type": "text", "value": "This is an overlay entry."},
+                    {
+                      "type": "flex",
+                      "direction": "horizontal",
+                      "children": [
+                        {
+                          "type": "textfield",
+                          "value": data["data"]["textfieldValue"],
+                          "label": "Quel est votre Prénom ?",
+                          "hintText": "Pascal",
+                          "minLines": 3,
+                          "maxLines": 5,
+                          "size": "large",
+                          "onChanged": {"code": "CodeA"}
+                        },
+                        {"type": "text", "value": data["data"]["textfieldValue"]}
+                      ]
+                    },
+                    {
+                      "type": "button",
+                      "text": "New page",
+                      "onPressed": {
+                        "code": "NewPage",
+                      }
+                    },
+                    {
+                      "type": "button",
+                      "text": "Close",
+                      "onPressed": {
+                        "code": "hideOverlay",
+                      }
+                    }
+                  ],
+                },
+              },
+            ]
+          },
+        }
+      },
+      {
+        "type": "overlayEntry",
+        "showOverlay": data["data"].containsKey("showOverlay2") ? data["data"]["showOverlay2"] : true,
+        "child": {
+          "type": "container",
+          "decoration": {
+            "color": 0x44000000,
+          },
+          "child": {
+            "type": "flex",
+            "mainAxisAlignment": "center",
+            "crossAxisAlignment": "center",
+            "direction": "vertical",
+            "children": [
+              {
+                "type": "container",
+                "decoration": {"color": 0xFFFFFFFF},
+                "padding": {
+                  "left": 2,
+                  "right": 2,
+                  "top": 2,
+                  "bottom": 2,
+                },
+                "child": {
+                  "type": "flex",
+                  "crossAxisAlignment": "center",
+                  "direction": "vertical",
+                  "spacing": 2,
+                  "children": [
+                    {
+                      "type": "text",
+                      "value": "This is an overlay.",
+                    },
+                    {"type": "text", "value": "You can close it by clicking on the button."},
+                    {
+                      "type": "button",
+                      "text": "Close",
+                      "onPressed": {
+                        "code": "hideOverlay2",
+                      }
+                    }
+                  ]
+                }
+              },
+            ]
+          },
+        }
+      }
+    ];
+  }
+
+  Map<String, dynamic> getPage(String pageName, Map<String, dynamic> data) {
+    Map<String, dynamic> page = {};
+
+    switch (pageName) {
+      case "home":
+        page = homePage();
+        break;
+      case "newPage":
+        page = newPage();
+        break;
+      default:
+        page = homePage();
+    }
+
+    return {
+      "type": "flex",
+      "children": [...getOverlays(data), page]
+    };
+  }
+
+  Map<String, dynamic> homePage() {
+    return {
+      "type": "flex",
+      "fillParent": true,
+      "mainAxisAlignment": "center",
+      "crossAxisAlignment": "center",
+      "children": [
+        {
+          "type": "flex",
+          "mainAxisAlignment": "center",
+          "crossAxisAlignment": "center",
+          "direction": "vertical",
+          "spacing": 2,
+          "fillParent": true,
+          "children": [
+            {
+              "type": "text",
+              "value": "You can show another overlay by clicking on the button below.",
+            },
+            {
+              "type": "button",
+              "text": "Show",
+              "onPressed": {
+                "code": "showOverlay",
+              }
+            },
+          ]
+        },
+      ]
+    };
+  }
+
+  Map<String, dynamic> newPage() {
+    return {
+      "type": "flex",
+      "fillParent": true,
+      "mainAxisAlignment": "center",
+      "crossAxisAlignment": "center",
+      "children": [
+        {
+          "type": "flex",
+          "mainAxisAlignment": "center",
+          "crossAxisAlignment": "center",
+          "direction": "vertical",
+          "spacing": 2,
+          "fillParent": true,
+          "children": [
+            {
+              "type": "text",
+              "value": "This is another page.",
+            },
+            {
+              "type": "button",
+              "text": "Go back to the first page",
+              "onPressed": {
+                "code": "InitData",
+              }
+            },
+          ]
+        },
+      ]
+    };
   }
 }
