52d77ee9cc946837f67c150f4da6bb377dff51db
Jonas Martinez <36544012+jonas-martinez@users.noreply.github.com>
2022-05-04 13:39:52 +0200

    feat: Add TextAlign property to Text (#143)

--- examples/showcase/lib/pages/lenra_text_page.dart
+++ examples/showcase/lib/pages/lenra_text_page.dart
@@ -14,25 +14,48 @@ class _LenraTextPageState extends UiBuilderState<LenraTextPage, bool> {
   Map<String, dynamic> getUi(dynamic data) {
     return {
       "root": {
-        "type": "text",
-        "value": "Test",
-        "style": {
-          "color": 0xFFFF0000,
-          "fontSize": 20,
-          "fontWeight": "bold",
-          "fontStyle": "italic",
-          "textDecoration": "underline",
-        }
-        // "children": [
-        //   {
-        //     "type": "text",
-        //     "value": "Foo",
-        //     "children": [
-        //       {"type": "text", "value": "Baz"}
-        //     ]
-        //   },
-        //   {"type": "text", "value": "Bar"}
-        // ]
+        "type": "flex",
+        "direction": "vertical",
+        "spacing": "4",
+        "children": [
+          {
+            "type": "text",
+            "value": "Test",
+            "style": {
+              "color": 0xFFFF0000,
+              "fontSize": 20,
+              "fontWeight": "bold",
+              "fontStyle": "italic",
+              "textDecoration": "underline",
+            },
+          },
+          {
+            "type": "container",
+            "constraints": {
+              "maxWidth": 60,
+              "minWidth": 60,
+              "maxHeight": 200,
+            },
+            "child": {
+              "type": "text",
+              "value": "This is a centered text.",
+              "textAlign": "center",
+            }
+          },
+          {
+            "type": "container",
+            "constraints": {
+              "maxWidth": 100,
+              "minWidth": 100,
+              "maxHeight": 200,
+            },
+            "child": {
+              "type": "text",
+              "value": "This is a justified text. This is a justified text.",
+              "textAlign": "justify",
+            }
+          }
+        ]
       }
     };
   }
