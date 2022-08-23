913d9ce8ba273dca3af19f151124426cc6299a52
jonas-martinez <cbobbxta.fr@gmail.com>
2022-08-23 14:49:31 +0200

    Added tests for slider form component

---
+++ test/components/form/lenra_form_slider_test.dart
@@ -0,0 +1,297 @@
+import 'package:flutter/material.dart';
+import 'package:lenra_components/component/lenra_button.dart';
+import 'package:lenra_components/component/lenra_slider.dart';
+import 'package:lenra_ui_runner/components/events/data/value_data.dart';
+import 'package:lenra_ui_runner/components/events/event.dart';
+import 'package:lenra_ui_runner/lenra_widget.dart';
+import 'package:lenra_ui_runner/widget_model.dart';
+import 'package:provider/provider.dart';
+import '../../test_helper.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+BuildContext? context;
+
+Widget createBaseFormTestWidget(bool Function(Event) onNotification) {
+  return createBaseTestWidgets(
+    child: NotificationListener(
+      child: Builder(
+        builder: (BuildContext ctx) {
+          context = ctx;
+
+          return LenraWidget(
+            buildErrorPage: (_ctx, _e) => Text("error"),
+            showSnackBar: (_ctx, _e) => {},
+          );
+        },
+      ),
+      onNotification: onNotification,
+    ),
+  );
+}
+
+Map<String, dynamic> createBaseFormUi(List<Map<String, dynamic>> children) {
+  return {
+    "root": {
+      "type": "form",
+      "onSubmit": {"code": "submitted"},
+      "child": {"type": "flex", "children": children}
+    }
+  };
+}
+
+void main() {
+  testWidgets(
+    'LenraForm Slider should work properly when pressed',
+    (WidgetTester tester) async {
+      bool hasBeenNotified = false;
+
+      await tester.pumpWidget(
+        createBaseFormTestWidget((e) {
+          if (e.code == "submitted") {
+            expect((e.data as ValueData).value, {
+              "sliderValue": 0.5,
+            });
+          }
+          hasBeenNotified = true;
+          return false;
+        }),
+      );
+
+      Map<String, dynamic> ui = createBaseFormUi(
+        [
+          {
+            "type": "slider",
+            "value": 0.0,
+            "max": 1.0,
+            "min": 0.0,
+            "name": "sliderValue",
+            "onChanged": {"code": "changed"}
+          },
+          {
+            "type": "button",
+            "text": "Submit",
+            "submit": true,
+          },
+        ],
+      );
+
+      context!.read<WidgetModel>().replaceUi(ui);
+
+      await tester.pump();
+      await tester.tap(find.byType(LenraSlider));
+      await tester.tap(find.byType(LenraButton));
+      expect(hasBeenNotified, true);
+    },
+  );
+
+  testWidgets(
+    'LenraForm Slider should work properly when pressed without onPressed property',
+    (WidgetTester tester) async {
+      bool hasBeenNotified = false;
+
+      await tester.pumpWidget(
+        createBaseFormTestWidget((e) {
+          if (e.code == "submitted") {
+            expect((e.data as ValueData).value, {
+              "sliderValue": 0.5,
+            });
+          }
+          hasBeenNotified = true;
+          return false;
+        }),
+      );
+
+      Map<String, dynamic> ui = createBaseFormUi(
+        [
+          {
+            "type": "slider",
+            "value": 0.0,
+            "max": 1.0,
+            "min": 0.0,
+            "name": "sliderValue",
+          },
+          {
+            "type": "button",
+            "text": "Submit",
+            "submit": true,
+          },
+        ],
+      );
+
+      context!.read<WidgetModel>().replaceUi(ui);
+
+      await tester.pump();
+      await tester.tap(find.byType(LenraSlider));
+      await tester.tap(find.byType(LenraButton));
+      expect(hasBeenNotified, true);
+    },
+  );
+
+  testWidgets('LenraForm Slider should not update form value when `name` property is not set',
+      (WidgetTester tester) async {
+    bool hasBeenNotified = false;
+
+    await tester.pumpWidget(
+      createBaseFormTestWidget((e) {
+        if (e.code == "submitted") {
+          // Expected value of Form should be empty
+          expect((e.data as ValueData).value, {});
+        }
+        hasBeenNotified = true;
+        return false;
+      }),
+    );
+
+    Map<String, dynamic> ui = createBaseFormUi([
+      {
+        "type": "slider",
+        "value": 0.0,
+        "max": 1.0,
+        "min": 0.0,
+      },
+      {
+        "type": "button",
+        "text": "Submit",
+        "submit": true,
+      }
+    ]);
+
+    context!.read<WidgetModel>().replaceUi(ui);
+
+    await tester.pump();
+    await tester.tap(find.byType(LenraSlider));
+    await tester.tap(find.byType(LenraButton));
+    expect(hasBeenNotified, true);
+  });
+
+  testWidgets(
+    'LenraForm Slider should not crash when `name` property is set outside of a Form',
+    (WidgetTester tester) async {
+      bool hasBeenNotified = false;
+
+      await tester.pumpWidget(
+        createBaseFormTestWidget((e) {
+          if (e.code == "submitted") {
+            expect((e.data as ValueData).value, {});
+          }
+          hasBeenNotified = true;
+          return false;
+        }),
+      );
+
+      Map<String, dynamic> ui = {
+        "root": {
+          "type": "flex",
+          "children": [
+            {
+              "type": "slider",
+              "value": 0.0,
+              "max": 1.0,
+              "min": 0.0,
+              "name": "sliderValue",
+            },
+            {
+              "type": "button",
+              "text": "Submit",
+              "submit": true,
+            }
+          ]
+        }
+      };
+
+      context!.read<WidgetModel>().replaceUi(ui);
+
+      await tester.pump();
+      await tester.tap(find.byType(LenraSlider));
+      await tester.tap(find.byType(LenraButton));
+      expect(hasBeenNotified, false);
+    },
+  );
+
+  testWidgets(
+    'LenraForm Slider sends default value to form when not clicked',
+    (WidgetTester tester) async {
+      bool hasBeenNotified = false;
+
+      await tester.pumpWidget(
+        createBaseFormTestWidget((e) {
+          if (e.code == "submitted") {
+            expect((e.data as ValueData).value, {
+              "sliderValue": 0.0,
+            });
+          }
+
+          hasBeenNotified = true;
+          return false;
+        }),
+      );
+
+      Map<String, dynamic> ui = createBaseFormUi([
+        {
+          "type": "slider",
+          "value": 0.0,
+          "max": 1.0,
+          "min": 0.0,
+          "name": "sliderValue",
+        },
+        {
+          "type": "button",
+          "text": "Submit",
+          "submit": true,
+        }
+      ]);
+
+      context!.read<WidgetModel>().replaceUi(ui);
+
+      await tester.pump();
+      await tester.tap(find.byType(LenraButton));
+      expect(hasBeenNotified, true);
+    },
+  );
+
+  testWidgets(
+    'LenraForm Slider properly executes listener when inside a Form',
+    (WidgetTester tester) async {
+      bool hasBeenNotified = false;
+      bool hasEnteredListener = false;
+
+      await tester.pumpWidget(
+        createBaseFormTestWidget((e) {
+          if (e.code == "submitted") {
+            expect((e.data as ValueData).value, {
+              "sliderValue": 0.5,
+            });
+          } else if (e.code == "changed") {
+            hasEnteredListener = true;
+          }
+          hasBeenNotified = true;
+          return false;
+        }),
+      );
+
+      Map<String, dynamic> ui = createBaseFormUi([
+        {
+          "type": "slider",
+          "value": 0.0,
+          "max": 1.0,
+          "min": 0.0,
+          "name": "sliderValue",
+          "onChanged": {"code": "changed"}
+        },
+        {
+          "type": "button",
+          "text": "Submit",
+          "submit": true,
+        }
+      ]);
+
+      context!.read<WidgetModel>().replaceUi(ui);
+
+      await tester.pump();
+      await tester.tap(find.byType(LenraSlider));
+      await tester.tap(find.byType(LenraButton));
+      expect(hasBeenNotified, true);
+      expect(hasEnteredListener, true);
+    },
+  );
+}
