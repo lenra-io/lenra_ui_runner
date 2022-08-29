f3ec28ec79eeb38106b078abf6bba71d9bcbef89
Jonas Martinez <36544012+jonas-martinez@users.noreply.github.com>
2022-08-29 14:34:10 +0200

    feat: Match client-app changes (#170)

--- lib/app.dart
+++ lib/app.dart
@@ -3,6 +3,7 @@ import 'package:lenra_ui_runner/lenra_application_model.dart';
 import 'package:lenra_ui_runner/lenra_ui_controller.dart';
 import 'package:lenra_ui_runner/models/channel_model.dart';
 import 'package:lenra_ui_runner/models/client_widget_model.dart';
+import 'package:lenra_ui_runner/models/context_model.dart';
 import 'package:lenra_ui_runner/models/socket_model.dart';
 import 'package:flutter/material.dart';
 import 'package:lenra_ui_runner/widget_model.dart';
@@ -24,15 +25,17 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider<ContextModel>(create: (context) => ContextModel()),
         ChangeNotifierProvider<UserApplicationModel>(create: (context) => UserApplicationModel()),
         ChangeNotifierProvider<LenraApplicationModel>(
           create: (context) => LenraApplicationModel(httpEndpoint, appName, accessToken),
         ),
-        ChangeNotifierProxyProvider<SocketModel, ChannelModel>(
-          create: (context) => ChannelModel(socketModel: context.read<SocketModel>()),
-          update: (_, socketModel, channelModel) {
+        ChangeNotifierProxyProvider2<SocketModel, ContextModel, ChannelModel>(
+          create: (context) =>
+              ChannelModel(socketModel: context.read<SocketModel>(), contextModel: context.read<ContextModel>()),
+          update: (_, socketModel, contextModel, channelModel) {
             if (channelModel == null) {
-              return ChannelModel(socketModel: socketModel);
+              return ChannelModel(socketModel: socketModel, contextModel: contextModel);
             }
             return channelModel.update(socketModel);
           },
@@ -43,8 +46,12 @@ class App extends StatelessWidget {
         ),
       ],
       builder: (BuildContext context, _) {
-        context.read<ChannelModel>().createChannel(appName);
-        (context.read<WidgetModel>() as ClientWidgetModel).setupListeners();
+        WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+          context.read<ContextModel>().mediaQueryData = MediaQuery.of(context);
+          //The model calls are in the postframe callback because the mediaquerydata is not set until the first frame
+          context.read<ChannelModel>().createChannel(appName);
+          (context.read<WidgetModel>() as ClientWidgetModel).setupListeners();
+        });
 
         return const LenraUiController();
       },
