77ac347ece6c2d9efaae04ab7d1f65e90d7cfbf4
jonas-martinez <cbobbxta.fr@gmail.com>
2023-04-25 17:16:53 +0200

    Fix more tests

--- test/components/form/lenra_form_button_test.dart
+++ test/components/form/lenra_form_button_test.dart
@@ -1,165 +1,150 @@
-// import 'package:flutter/material.dart';
-// import 'package:lenra_components/component/lenra_button.dart';
-// import 'package:lenra_ui_runner/io_components/lenra_widget.dart';
-// import 'package:lenra_ui_runner/models/channel_model.dart';
-// import 'package:provider/provider.dart';
-// import '../../mock_channel_model.dart';
-// import '../../test_helper.dart';
-// import 'package:flutter_test/flutter_test.dart';
-
-// BuildContext? context;
-
-// Widget createBaseFormTestWidget(Function callback) {
-//   return createBaseTestWidgets(
-//     child: Builder(
-//       builder: (BuildContext ctx) {
-//         context = ctx;
-//         (Provider.of<ChannelModel>(ctx, listen: false) as MockChannelModel).setCallBack(callback);
-
-//         return LenraWidget(
-//           buildErrorPage: (_ctx, _e) => Text("error"),
-//           showSnackBar: (_ctx, _e) => {},
-//         );
-//       },
-//     ),
-//   );
-// }
-
-// Map<String, dynamic> createBaseFormUi(List<Map<String, dynamic>> children) {
-//   return {
-//     "root": {
-//       "type": "form",
-//       "onSubmit": {"code": "submitted"},
-//       "child": {"type": "flex", "children": children}
-//     }
-//   };
-// }
+import 'package:lenra_components/component/lenra_button.dart';
+import '../../test_helper.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-//   testWidgets(
-//     'LenraForm Button should submit form properly when pressed',
-//     (WidgetTester tester) async {
-//       bool hasBeenNotified = false;
-//       bool hasSubmittedForm = false;
+  testWidgets(
+    'LenraForm Button should submit form properly when pressed',
+    (WidgetTester tester) async {
+      bool hasBeenNotified = false;
+      bool hasSubmittedForm = false;
 
-//       await tester.pumpWidget(
-//         createBaseFormTestWidget((event) {
-//           if (event["code"] == "submitted") {
-//             expect(event["event"]["value"], {});
-//             hasSubmittedForm = true;
-//           }
-//           hasBeenNotified = true;
-//           return false;
-//         }),
-//       );
+      await tester.pumpWidget(
+        createBaseTestWidgets(
+          ui: {
+            "root": {
+              "type": "form",
+              "onSubmit": {"code": "submitted"},
+              "child": {
+                "type": "flex",
+                "children": [
+                  {
+                    "type": "button",
+                    "text": "Submit",
+                    "submit": true,
+                  },
+                ]
+              }
+            }
+          },
+          sendEventFn: (event) {
+            if (event.code == "submitted") {
+              expect(event.data.toMap()["value"], {});
+              hasSubmittedForm = true;
+            }
+            hasBeenNotified = true;
+            return Future.value(true);
+          },
+        ),
+      );
 
-//       Map<String, dynamic> ui = createBaseFormUi(
-//         [
-//           {
-//             "type": "button",
-//             "text": "Submit",
-//             "submit": true,
-//           },
-//         ],
-//       );
+      await tester.pump();
+      await tester.tap(find.byType(LenraButton));
+      expect(hasBeenNotified, true);
+      expect(hasSubmittedForm, true);
+    },
+  );
 
-//       context!.read<ViewModel>().replaceUi(ui);
+  testWidgets(
+    'LenraForm Button should not crash if submit property is true outside of a form',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(
+        createBaseTestWidgets(
+          ui: {
+            "root": {
+              "type": "button",
+              "text": "Submit",
+              "submit": true,
+            }
+          },
+          sendEventFn: (_) {
+            return Future.value(true);
+          },
+        ),
+      );
 
-//       await tester.pump();
-//       await tester.tap(find.byType(LenraButton));
-//       expect(hasBeenNotified, true);
-//       expect(hasSubmittedForm, true);
-//     },
-//   );
+      await tester.pump();
+      await tester.tap(find.byType(LenraButton));
+    },
+  );
 
-//   testWidgets(
-//     'LenraForm Button should not crash if submit property is true outside of a form',
-//     (WidgetTester tester) async {
-//       await tester.pumpWidget(
-//         createBaseFormTestWidget((e) => false),
-//       );
+  testWidgets(
+    'LenraForm Button should not submit form if submit property is false inside of a Form',
+    (WidgetTester tester) async {
+      bool hasSubmittedForm = false;
 
-//       Map<String, dynamic> ui = {
-//         "root": {
-//           "type": "button",
-//           "text": "Submit",
-//           "submit": true,
-//         }
-//       };
+      await tester.pumpWidget(
+        createBaseTestWidgets(
+          ui: {
+            "root": {
+              "type": "form",
+              "onSubmit": {"code": "submitted"},
+              "child": {
+                "type": "flex",
+                "children": [
+                  {
+                    "type": "button",
+                    "text": "Submit",
+                  },
+                ]
+              }
+            }
+          },
+          sendEventFn: (event) {
+            if (event.code == "submitted") {
+              hasSubmittedForm = true;
+            }
+            return Future.value(true);
+          },
+        ),
+      );
 
-//       context!.read<ViewModel>().replaceUi(ui);
+      await tester.pump();
+      await tester.tap(find.byType(LenraButton));
+      expect(hasSubmittedForm, false);
+    },
+  );
 
-//       await tester.pump();
-//       await tester.tap(find.byType(LenraButton));
-//     },
-//   );
+  testWidgets(
+    'LenraForm Button listener is properly triggered when set and when submit property is true in a Form',
+    (WidgetTester tester) async {
+      bool hasSubmittedForm = false;
+      bool buttonPressed = false;
 
-//   testWidgets(
-//     'LenraForm Button should not submit form if submit property is false inside of a Form',
-//     (WidgetTester tester) async {
-//       bool hasSubmittedForm = false;
+      await tester.pumpWidget(
+        createBaseTestWidgets(
+          ui: {
+            "root": {
+              "type": "form",
+              "onSubmit": {"code": "submitted"},
+              "child": {
+                "type": "flex",
+                "children": [
+                  {
+                    "type": "button",
+                    "text": "Submit",
+                    "submit": true,
+                    "onPressed": {"code": "pressed"}
+                  },
+                ]
+              }
+            }
+          },
+          sendEventFn: (event) {
+            if (event.code == "submitted") {
+              hasSubmittedForm = true;
+            } else if (event.code == "pressed") {
+              buttonPressed = true;
+            }
+            return Future.value(true);
+          },
+        ),
+      );
 
-//       await tester.pumpWidget(
-//         createBaseFormTestWidget((e) {
-//           if (e["code"] == "submitted") {
-//             hasSubmittedForm = true;
-//           }
-//           return false;
-//         }),
-//       );
-
-//       Map<String, dynamic> ui = createBaseFormUi(
-//         [
-//           {
-//             "type": "button",
-//             "text": "Submit",
-//           },
-//         ],
-//       );
-
-//       context!.read<ViewModel>().replaceUi(ui);
-
-//       await tester.pump();
-//       await tester.tap(find.byType(LenraButton));
-//       expect(hasSubmittedForm, false);
-//     },
-//   );
-
-//   testWidgets(
-//     'LenraForm Button listener is properly triggered when set and when submit property is true in a Form',
-//     (WidgetTester tester) async {
-//       bool hasSubmittedForm = false;
-//       bool buttonPressed = false;
-
-//       await tester.pumpWidget(
-//         createBaseFormTestWidget((e) {
-//           if (e["code"] == "submitted") {
-//             hasSubmittedForm = true;
-//           } else if (e["code"] == "pressed") {
-//             buttonPressed = true;
-//           }
-
-//           return false;
-//         }),
-//       );
-
-//       Map<String, dynamic> ui = createBaseFormUi(
-//         [
-//           {
-//             "type": "button",
-//             "text": "Submit",
-//             "submit": true,
-//             "onPressed": {"code": "pressed"}
-//           },
-//         ],
-//       );
-
-//       context!.read<ViewModel>().replaceUi(ui);
-
-//       await tester.pump();
-//       await tester.tap(find.byType(LenraButton));
-//       expect(hasSubmittedForm, true);
-//       expect(buttonPressed, true);
-//     },
-//   );
+      await tester.pump();
+      await tester.tap(find.byType(LenraButton));
+      expect(hasSubmittedForm, true);
+      expect(buttonPressed, true);
+    },
+  );
 }
--- test/components/lenra_toggle_test.dart
+++ test/components/lenra_toggle_test.dart
@@ -1,108 +1,80 @@
-// import 'package:flutter/gestures.dart';
-// import 'package:flutter/material.dart';
-// import 'package:lenra_components/component/lenra_toggle.dart';
-// import 'package:lenra_ui_runner/models/channel_model.dart';
-
-// import 'package:provider/src/provider.dart';
-// import '../mock_channel_model.dart';
-// import "../test_helper.dart";
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:lenra_ui_runner/lenra_ui_runner.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:lenra_components/component/lenra_toggle.dart';
+import "../test_helper.dart";
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-//   testWidgets('LenraToggle property', (WidgetTester tester) async {
-//     BuildContext? _context;
+  testWidgets('LenraToggle property', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      createBaseTestWidgets(
+        ui: {
+          "root": {
+            "type": "toggle",
+            "value": true,
+            "onPressed": {"code": "YourCode"},
+            "style": {
+              "activeTrackColor": 0xFF000000,
+              "focusColor": 0xFF000000,
+              "inactiveThumbColor": 0xFF000000,
+              "hoverColor": 0xFF000000,
+              "materialTapTargetSize": "padded",
+            },
+            "autofocus": true,
+            "splashRadius": 2.0,
+            "dragStartBehavior": "down",
+          }
+        },
+        sendEventFn: (_) {
+          return Future.value(true);
+        },
+      ),
+    );
 
-//     await tester.pumpWidget(
-//       createBaseTestWidgets(
-//         child: Builder(
-//           builder: (BuildContext context) {
-//             _context = context;
+    await tester.pump();
 
-//             return LenraWidget(
-//               buildErrorPage: (_ctx, _e) => Text("error"),
-//               showSnackBar: (_ctx, _e) => {},
-//             );
-//           },
-//         ),
-//       ),
-//     );
+    var finderToggle = find.byType(LenraToggle);
+    var toggle = tester.widget<LenraToggle>(finderToggle);
+    var style = toggle.style;
+    expect(finderToggle, findsOneWidget);
+    expect(toggle.value, true);
+    expect(toggle.autofocus, true);
+    expect(toggle.splashRadius, 2.0);
+    expect(toggle.dragStartBehavior, DragStartBehavior.down);
+    expect(style!.activeTrackColor, Colors.black);
+    expect(style.focusColor, Colors.black);
+    expect(style.inactiveThumbColor, Colors.black);
+    expect(style.hoverColor, Colors.black);
+    expect(style.materialTapTargetSize, MaterialTapTargetSize.padded);
+  });
 
-//     Map<String, dynamic> ui = {
-//       "root": {
-//         "type": "toggle",
-//         "value": true,
-//         "onPressed": {"code": "YourCode"},
-//         "style": {
-//           "activeTrackColor": 0xFF000000,
-//           "focusColor": 0xFF000000,
-//           "inactiveThumbColor": 0xFF000000,
-//           "hoverColor": 0xFF000000,
-//           "materialTapTargetSize": "padded",
-//         },
-//         "autofocus": true,
-//         "splashRadius": 2.0,
-//         "dragStartBehavior": "down",
-//       }
-//     };
+  testWidgets('LenraToggle test onPressed', (WidgetTester tester) async {
+    bool hasBeenNotified = false;
 
-//     _context!.read<ViewModel>().replaceUi(ui);
-//     await tester.pump();
+    await tester.pumpWidget(
+      createBaseTestWidgets(
+        ui: {
+          "root": {
+            "type": "toggle",
+            "value": true,
+            "onPressed": {"code": "YourCode"},
+            "autofocus": true,
+            "splashRadius": 2.0,
+            "dragStartBehavior": "down",
+          }
+        },
+        sendEventFn: (_) {
+          hasBeenNotified = true;
+          return Future.value(true);
+        },
+      ),
+    );
 
-//     var finderToggle = find.byType(LenraToggle);
-//     var toggle = tester.widget<LenraToggle>(finderToggle);
-//     var style = toggle.style;
-//     expect(finderToggle, findsOneWidget);
-//     expect(toggle.value, true);
-//     expect(toggle.autofocus, true);
-//     expect(toggle.splashRadius, 2.0);
-//     expect(toggle.dragStartBehavior, DragStartBehavior.down);
-//     expect(style!.activeTrackColor, Colors.black);
-//     expect(style.focusColor, Colors.black);
-//     expect(style.inactiveThumbColor, Colors.black);
-//     expect(style.hoverColor, Colors.black);
-//     expect(style.materialTapTargetSize, MaterialTapTargetSize.padded);
-//   });
+    await tester.pump();
 
-//   testWidgets('LenraToggle test onPressed', (WidgetTester tester) async {
-//     BuildContext? _context;
-//     bool hasBeenNotified = false;
-
-//     await tester.pumpWidget(
-//       createBaseTestWidgets(
-//         child: Builder(
-//           builder: (BuildContext context) {
-//             _context = context;
-//             (Provider.of<ChannelModel>(context, listen: false) as MockChannelModel).setCallBack((value) {
-//               hasBeenNotified = true;
-//             });
-
-//             return LenraWidget(
-//               buildErrorPage: (_ctx, _e) => Text("error"),
-//               showSnackBar: (_ctx, _e) => {},
-//             );
-//           },
-//         ),
-//       ),
-//     );
-
-//     Map<String, dynamic> ui = {
-//       "root": {
-//         "type": "toggle",
-//         "value": true,
-//         "onPressed": {"code": "YourCode"},
-//         "autofocus": true,
-//         "splashRadius": 2.0,
-//         "dragStartBehavior": "down",
-//       }
-//     };
-
-//     _context!.read<ViewModel>().replaceUi(ui);
-//     await tester.pump();
-
-//     var toggle = find.byType(LenraToggle);
-//     await tester.tap(toggle);
-//     await tester.pump();
-//     expect(hasBeenNotified, true);
-//   });
+    var toggle = find.byType(LenraToggle);
+    await tester.tap(toggle);
+    await tester.pump();
+    expect(hasBeenNotified, true);
+  });
 }
