683f7a93960c80736d75ef9776f67ab4c4854036
jonas-martinez <cbobbxta.fr@gmail.com>
2023-04-26 11:50:33 +0200

    More tests

--- lib/components/lenra_textfield.dart
+++ lib/components/lenra_textfield.dart
@@ -9,7 +9,6 @@ import 'package:lenra_ui_runner/components/events/on_changed_event.dart';
 import 'package:lenra_ui_runner/components/lenra_form.dart';
 import 'package:lenra_ui_runner/components/listeners/listener.dart' as lenra;
 import 'package:lenra_ui_runner/io_components/lenra_route.dart';
-import 'package:lenra_ui_runner/models/channel_model.dart';
 import 'package:lenra_ui_runner/utils/lenra_text_field_style.dart';
 import 'package:provider/provider.dart';
 import '../../lenra_component_builder.dart';
